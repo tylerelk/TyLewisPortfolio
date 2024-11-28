568e57a0b07a0eab112d3d87563c6000ece06cd2
Tyler Lewis <tylerlewisrn@gmail.com>
2024-11-28 10:21:35 -0500

    Add animated background

--- TyLewisPortfolio/src/App.tsx
+++ TyLewisPortfolio/src/App.tsx
@@ -1,25 +1,10 @@
-import booBoo from "./assets/images/booboo.jpeg";
 import "./App.css";
+import Background from "./components/Background";
 
 function App() {
   return (
     <>
-      <div>
-        <a href='https://github.com/tylerelk' target='_blank'>
-          <img
-            src={booBoo}
-            style={{
-              width: "20%",
-              borderRadius: "20px",
-            }}
-          />
-        </a>
-      </div>
-      <h1>Ty Lewis</h1>
-      <div className='card'>
-        <p>Software Engineer</p>
-      </div>
-      <p className='read-the-docs'>Personal website coming soon.</p>
+      <Background />
     </>
   );
 }
---
+++ TyLewisPortfolio/src/components/Background.tsx
@@ -0,0 +1,51 @@
+import "./Background.css";
+
+function Background() {
+  return (
+    <>
+      <div className='background'>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
+  );
+}
+
+export default Background;
