8d886830324669f768f4320ee08115e6af3fa538
Tyler Lewis <tylerlewisrn@gmail.com>
2024-12-12 18:00:25 -0500

    Update image importing

---
+++ TyLewisPortfolio/src/assets/icons/tech/TechIcons.ts
@@ -0,0 +1,33 @@
+import AWS from "./aws.svg";
+import Azure from "./azure.svg";
+import Blazor from "./blazor.svg";
+import CSharp from "./csharp.svg";
+import CSS from "./css.svg";
+import DotNet from "./dotnet.svg";
+import Git from "./git.svg";
+import GitHub from "./github.svg";
+import GitLab from "./gitlab.svg";
+import HTML from "./html.svg";
+import JavaScript from "./javascript.svg";
+import React from "./react.svg";
+import SQL from "./sql.svg";
+import Tailwind from "./tailwind.svg";
+import TypeScript from "./typescript.svg";
+
+export default [
+  AWS,
+  Azure,
+  Blazor,
+  CSharp,
+  CSS,
+  DotNet,
+  Git,
+  GitHub,
+  GitLab,
+  HTML,
+  JavaScript,
+  React,
+  SQL,
+  Tailwind,
+  TypeScript,
+];
--- TyLewisPortfolio/src/components/TechSection.tsx
+++ TyLewisPortfolio/src/components/TechSection.tsx
@@ -1,14 +1,9 @@
 import "./TechSection.css";
 import Skills from "./Skills";
 import { isDesktop } from "react-device-detect";
+import Icons from "../assets/icons/tech/TechIcons";
 
 export default function TechSection() {
-  const icons = Object.values(
-    import.meta.glob("../assets/icons/tech/*.svg", {
-      eager: true,
-      query: "?raw",
-    })
-  );
   return (
     <>
       <p className='text-xl text-left'>Here's what I'm good at</p>
@@ -17,13 +12,14 @@ export default function TechSection() {
       <div
         className={`m-auto flex flex-wrap justify-center gap-2 ${isDesktop ? "w-9/12" : "w-full"}`}
       >
-        {icons.map((icon: any, index) => {
+        {Icons.map((icon: any, index) => {
           return (
             <div
               key={index}
-              dangerouslySetInnerHTML={{ __html: icon.default }}
               className={` flex justify-center items-center border-8 border-gray-100 rounded-full transition-all duration-200  ${isDesktop ? "iconSquare w-32 h-32 grayscale p-8 hover:bg-white hover:border-0" : "w-16 h-16"}`}
-            ></div>
+            >
+              <img src={icon} alt='' />
+            </div>
           );
         })}
       </div>
