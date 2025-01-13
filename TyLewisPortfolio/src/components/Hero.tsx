648e90c492fdf994017b6278863e96a6fd6e3e5d
Tyler Lewis <tylerlewisrn@gmail.com>
2025-01-13 18:40:37 -0500

    Various style updates

--- TyLewisPortfolio/src/components/Hero.tsx
+++ TyLewisPortfolio/src/components/Hero.tsx
@@ -1,30 +1,49 @@
+import { useEffect, useState } from "react";
 import { isDesktop, isMobile } from "react-device-detect";
 import _meAndChloe from "../assets/images/ty-and-chloe-line-art.svg";
 import Bestagon from "./Bestagon";
-import _resume from "../assets/data/resume.json";
+import _bullets from "../assets/data/aboutme.json";
 
 export default function Hero() {
+  const [isShifted, setIsShifted] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShifted(true);
+    }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
+  });
   return (
     <>
       <div
         id='hero'
-        className={`w-screen h-screen flex items-center justify-center relative ${isDesktop ? "bg-hero-background" : "bg-gradient-to-b from-black via-gray-800 to-transparent"}`}
+        className={`w-screen h-screen flex items-center justify-center relative ${isDesktop ? "bg-hero-background" : ""}`}
       >
         {isDesktop && <Bestagon />}
         <div
-          className={`w-7/12 bg-card-background rounded-3xl shadow-2xl shadow-teal-400 p-6 ${isMobile ? 'animate-fade-in' : ''}`}
+          className={`bg-card-background rounded-3xl shadow-xl shadow-teal-400 p-10 flex ${isMobile ? "animate-fade-in" : ""}`}
         >
-          <div className='flex flex-col items-center'>
-            <div className='flex gap-2'>
+          <div
+            className={`flex items-center ${isMobile ? "flex-col" : "gap-10"}`}
+          >
+            <div className={`flex flex-col justify-around`}>
               <h1 className='text-9xl font-black text-transparent bg-clip-text bg-gradient-to-b from-pink-500 to-teal-400'>
                 Hi!
               </h1>
+              <h2 className='text-7xl font-thin text-transparent bg-clip-text bg-gradient-to-b from-teal-400 to-pink-500'>
+                I'm Ty
+              </h2>
             </div>
-            <p
-              className={`m-auto text-lg text-black ${isDesktop ? "w-3/4" : "w-full"}`}
+            <ul
+              className={`mt-5 text-2xl font-black font-mono text-right transform transition-all duration-1000 ease-in-out ${isShifted ? "opacity-100" : "opacity-0"}`}
             >
-              {_resume.sections.summary.content}
-            </p>
+              <span className='border-b-2'>a Software Engineer</span>
+              {isDesktop &&
+                _bullets["hero-section"].map((b) => {
+                  return <li className='font-thin text-sm leading-6'>{b}</li>;
+                })}
+            </ul>
           </div>
         </div>
       </div>
--- TyLewisPortfolio/src/components/Loader.tsx
+++ TyLewisPortfolio/src/components/Loader.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, ReactNode } from "react";
 import "./Loader.css";
 import { isDesktop, isMobile } from "react-device-detect";
+import icon from "../assets/icons/loader-icon.apng";
 
 interface LoaderProps {
   children: ReactNode;
@@ -21,9 +22,10 @@ const Loader: React.FC<LoaderProps> = ({ children }) => {
     <>
       <div>
         {isLoading && isDesktop ? (
-          <div className='fixed w-full h-full bg-black flex justify-center items-center transition-all duration-200'>
+          <div className='fixed w-full h-full bg-black flex flex-col justify-center items-center transition-all duration-200'>
+            <img src={icon} alt='' />
             <h1
-              className={`bg-gradient-to-b from-teal-300 to-yellow-500 text-transparent bg-clip-text ${isMobile ? "text-3xl" : "text-9xl"}`}
+              className={`bg-gradient-to-b from-purple-600 to-teal-500 text-transparent bg-clip-text ${isMobile ? "text-md" : "text-3xl"}`}
             >
               Loading...
             </h1>
